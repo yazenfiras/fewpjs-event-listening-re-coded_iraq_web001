25be3ef777cf24acd9f48e030e0be3877f8a2b59
yazen firas <psyazen3@gmail.com>
2020-06-02 13:08:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const input = document.querySelector('input');
 
 function addingEventListener() {
-  input.addEventListener('click', function(event) {
+  input.addEventListener('click', function() {
     return "clicked"
   });
 }
